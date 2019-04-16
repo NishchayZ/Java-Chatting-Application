9959805c922bd7d03b0e0cbb679c01c673ee6433
Nishchay Zinzore <37393250+nishchayzinzore@users.noreply.github.com>
2019-04-16 08:27:38 +0530

    This is a Chatting Application with File Transmission.

---
+++ src/application_chat/Application_Chat.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application_chat;
+
+/**
+ *
+ * @author Asus
+ */
+public class Application_Chat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new ConnectionDetails().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
